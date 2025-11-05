0356c6d8e38ff604b5610219f89eaad12fa12992
Vicent Lundborg <v@lundborgs.de>
2025-11-05 12:58:58 +0100

    Update 2025-11-05 12:58:58

--- hosts/server/step-ca.nix
+++ hosts/server/step-ca.nix
@@ -2,7 +2,7 @@
   services.step-ca = {
     enable = true;
     settings = builtins.fromJSON( builtins.readFile ./ca.json );
-    address = "127.0.0.1";
+    address = "192.168.1.205";
     port = 8443;
     intermediatePasswordFile = "/var/lib/secrets/step-ca/intermediate_password";
   };
