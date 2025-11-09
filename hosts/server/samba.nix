66caa4e1cde39439cb3aecdd50c2f5c7defacef3
Vicent Lundborg <v@lundborgs.de>
2025-11-09 13:28:24 +0100

    Update 2025-11-09 13:28:24

--- hosts/server/samba.nix
+++ hosts/server/samba.nix
@@ -6,10 +6,20 @@
 	path = "/srv/shared/libary";
 	comment = "Network folder for ebooks";
 	browseable = true;
-	"read only" = false;
-	"guest ok" = true;
+	"read only" = "no";
+	"guest ok" = "no";
+	"valid users" = [ "smbuser" ];
+	"create mask" = "0664";
+      	"directory mask" = "0775";
+      	"force user" = "smbuser";
       };
     };
   };
   networking.firewall.allowedTCPPorts = [ 445 ];
+  
+  users.users.smbUser = {
+    isSystemUser = true;
+    group = "smbUser";
+  };
+  users.groups.smbUser = {};
 }
