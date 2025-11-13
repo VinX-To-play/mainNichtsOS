6db2203d44fca8da794b0cbdcf1c8e1f26862a32
VinX-To-play <v@lundborgs.de>
2025-11-13 13:11:25 +0100

    Update 2025-11-13 13:11:25

--- shared/hm-modules/kickstart.nixvim/plugins/lsp.nix
+++ shared/hm-modules/kickstart.nixvim/plugins/lsp.nix
@@ -42,6 +42,39 @@
       cspell
     ];
 
+    extraConfigLuaPost = ''
+      return {
+        {
+          "nvimtools/none-ls.nvim",
+          event = "VeryLazy",
+          dependencies = { "davidmh/cspell.nvim" },
+          opts = function(_, opts)
+            local cspell = require("cspell")
+            opts.sources = opts.sources or {}
+            table.insert(
+              opts.sources,
+              cspell.diagnostics.with({
+                diagnostics_postprocess = function(diagnostic)
+                  diagnostic.severity = vim.diagnostic.severity.HINT
+                end,
+              })
+            )
+            table.insert(opts.sources, cspell.code_actions)
+          end,
+        },
+      }
+    '';
+    
+    # extraConfigLuaPost = ''
+    #   local cspell = require('cspell')
+    #     require("null-ls").setup {
+    #         sources = {
+    #             cspell.diagnostics,
+    #             cspell.code_actions,
+    #         }
+    #     }
+    # '';
+
     # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
     autoGroups = {
       "kickstart-lsp-attach" = {
