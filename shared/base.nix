0bd3e390101c48f939ffc25bd1bf762ae451444d
VinX-To-play <v@lundborgs.de>
2025-11-14 16:08:05 +0100

    Update 2025-11-14 16:08:05

--- shared/base.nix
+++ shared/base.nix
@@ -101,10 +101,8 @@ boot.loader = {
   services.tailscale = {
     enable = true;
     extraSetFlags = [ "--accept-dns=false" ];
-    #extraDaemonFlags = [ "--login-server=https://headscale.swahnlabs.com/" ];
     extraUpFlags = [ "--login-server=https://headscale.swahnlabs.com/" ];
     authKeyFile = config.sops.secrets."services/tailscale/autkey".path;
-    #authKeyParameters.baseURL = "https://headscale.swahnlabs.com/";
     useRoutingFeatures =  lib.mkDefault "client";
   };
   
