e930a48910a6399f3a9f27e58f8d75544638ede5
VinX-To-play <v@lundborgs.de>
2025-12-05 12:06:00 +0100

    Update 2025-12-05 12:06:00

---
+++ shared/modules/sway/sway.nix
@@ -0,0 +1,16 @@
+{pkgs, ... }:
+{
+  # kanshi systemd service
+  systemd.user.services.kanshi = {
+    description = "kanshi daemon";
+    environment = {
+      WAYLAND_DISPLAY="wayland-1";
+      DISPLAY = ":0";
+    }; 
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+    };
+  };
+  
+}
