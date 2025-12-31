f0b5e235c7b9b3485c89ffae3f116fa827c6a841
Vicent Lundborg <v@lundborgs.de>
2025-12-31 12:25:40 +0100

    set ca to also listen to tailscale

--- hosts/server/step-ca.nix
+++ hosts/server/step-ca.nix
@@ -2,7 +2,7 @@
   services.step-ca = {
     enable = true;
     settings = builtins.fromJSON( builtins.readFile ./ca.json );
-    address = "192.168.1.205";
+    address = "0.0.0.0";
     port = 8443;
     intermediatePasswordFile = "/var/lib/secrets/step-ca/intermediate_password";
   };
