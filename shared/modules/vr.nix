1aab333df6ae1ef1739065d67ea1f5673141e60d
VinX-To-play <v@lundborgs.de>
2026-01-17 18:31:07 +0100

    Update 2026-01-17 18:31:07

--- shared/modules/vr.nix
+++ shared/modules/vr.nix
@@ -28,10 +28,16 @@
     forceDefaultRuntime = true;
     highPriority = true;
   };
-
-  environment.sessionVariables = {
-    SURVIVE_GLOBALSCENESOLVER = "0";
+  
+  systemd.user.services.monado.environment = {
+    #XRT_COMPOSITOR_FORCE_WAYLAND_DIRECT= "1";
+    STEAMVR_LH_ENABLE = "0";
+    XRT_COMPOSITOR_COMPUTE = "0";
+    WMR_HANDTRACKING = "0";
+    "IPC_EXIT_ON_DISCONNECT" = "1";
+    "SURVIVE_GLOBALSCENESOLVER" = "0";
   };
+
 
   programs.steam = {
     package = lib.mkForce ( pkgs.steam.override {
@@ -64,15 +70,6 @@
     })
   ];
 
-
-  systemd.user.services.monado.environment = {
-    XRT_COMPOSITOR_FORCE_WAYLAND_DIRECT= "1";
-    STEAMVR_LH_ENABLE = "0";
-    XRT_COMPOSITOR_COMPUTE = "1";
-    WMR_HANDTRACKING = "0";
-    "IPC_EXIT_ON_DISCONNECT" = "1";
-  };
-  
   hardware.graphics.extraPackages = with pkgs; [monado-vulkan-layers];
   
 }
