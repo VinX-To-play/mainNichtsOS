f273478d9e05e86f5bf7eaef28369b28ff58d8f9
VinX-To-play <v@lundborgs.de>
2026-02-04 10:49:36 +0100

    Update 2026-02-04 10:49:36

--- shared/hm-modules/bash.nix
+++ shared/hm-modules/bash.nix
@@ -4,14 +4,12 @@
   initExtra = ''
       get_git_info() {
         local branch
+        local git
         branch=$(git branch --show-current 2>/dev/null)
         if [ -n "$branch" ]; then
           # \001 and \002 tell Bash these are non-printing characters
-          echo -e "\001\e[1;35m\002  $branch \001\e[0m\002 |"
+          git=$"\001\e[1;35m\002  $branch \001\e[0m\002 |"
         fi
-      }
-
-      PROMPT_COMMAND=PS1_CMD1="$(get_git_info)"
 
       # Use $'\e...' so Bash interprets the escape codes
       BLUE=$'\e[1;34m'
@@ -22,7 +20,13 @@
       # 1. We use " " so the color variables (BLUE, etc.) expand now.
       # 2. We use \$ so the PS1_CMD1 variable expands LATER (every prompt).
       # 3. In Nix, we write ''$ to get a literal $ in the output file.
-      PS1="\n\[$BLUE\] \t \[$RESET\]| $PS1_CMD1 [$GREEN\]  \w \[$RESET\]\n\[$YELLOW\]λ \[$RESET\]"
+      PS1="\n\[$BLUE\] \t \[$RESET\]| $git \[$GREEN\]  \w \[$RESET\]\n\[$YELLOW\]λ \[$RESET\]"
+
+
+      }
+
+      PROMPT_COMMAND="$(get_git_info)"
+
 
 
     # Run fastfetch only in interactive shells
