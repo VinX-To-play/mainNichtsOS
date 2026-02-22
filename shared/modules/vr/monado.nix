f0a28efcf98b89400e30514402b0072fdb385fb8
VinX-To-play <v@lundborgs.de>
2026-02-22 16:54:50 +0100

    Update 2026-02-22 16:54:50

--- shared/modules/vr/monado.nix
+++ shared/modules/vr/monado.nix
@@ -6,10 +6,10 @@
     highPriority = true;
   };
 
-  systemd.services."monado".environment = {
+  systemd.user.services."monado".environment = {
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
-    XRT_FORCE_DRIVER = "steamvr";
+    XRT_DRIVER = "steamvr";
   };
 
 }
