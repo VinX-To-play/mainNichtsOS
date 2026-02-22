1b81be49b895e8d03de96e3e5926a0a9409fa5e1
VinX-To-play <v@lundborgs.de>
2026-02-22 17:03:33 +0100

    Update 2026-02-22 17:03:33

--- shared/modules/vr/monado.nix
+++ shared/modules/vr/monado.nix
@@ -12,4 +12,21 @@
     XRT_DRIVER = "steamvr";
   };
 
+  users.users.vincentl.extraGroups = ["video" "input"];
+
+  # AI udev rouls for Vive
+  services.udev.extraRules = ''
+    # HTC Vive headset & linkbox and related HTC USB devices
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", MODE="0666"
+
+    # Valve / Lighthouse / Watchman dongles
+    SUBSYSTEM=="usb", ATTR{idVendor}=="28de", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+
+    # hidraw access is usually the real blocker for VR devices on Linux
+    KERNEL=="hidraw*", ATTRS{idVendor}=="0bb4", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+  '';
+
 }
