96b6fcfe95e93cb5aae58aa868e1390f4027ad68
VinX-To-play <v@lundborgs.de>
2026-03-01 17:39:53 +0100

    Update 2026-03-01 17:39:53

--- hosts/main_desktop/configuration.nix
+++ hosts/main_desktop/configuration.nix
@@ -77,7 +77,7 @@
   };
  
   # set .config backup extansion for home manager
-  home-manager.backupFileExtension = "backup100";
+  home-manager.backupFileExtension = "backup101";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
