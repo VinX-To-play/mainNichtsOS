e3328b907f2b1d9275bb38c700517ca834e8104a
Vicent Lundborg <v@lundborgs.de>
2026-05-20 17:47:35 +0200

    Update 2026-05-20 17:47:34

--- hosts/server/open-webui.nix
+++ hosts/server/open-webui.nix
@@ -9,15 +9,24 @@
       ENABLE_OPENAI_API = "True";
       OPENAI_API_BASE_URL = "http://main.int:11343/v1";
       WEBUI_AUTH = "False";
+      CORIS_ALLOW_ORIGIN = "https://chat.slave.int";
+      WEBHOOK_URL = "chat.slave.int";
     };
   };
 
   services.nginx.virtualHosts."chat.slave.int" = {
     enableACME = true;
     forceSSL = true;
-
     locations."/" = {
         proxyPass = "http://${config.services.open-webui.host}:${toString config.services.open-webui.port}";
+        proxyWebsockets = true;
+        extraConfig = "
+          proxy_buffering off;
+          proxy_cache off;
+          proxy_read_timeout 1800;
+          proxy_send_timeout 1800;
+          proxy_connect_timeout 1800;
+        ";
     };
   };
 }
