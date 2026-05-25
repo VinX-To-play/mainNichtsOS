dc8daa86e5f5e56154730c782d8ca7fd87d60bf6
Vicent Lundborg <v@lundborgs.de>
2026-05-25 14:11:20 +0200

    Update 2026-05-25 14:11:20

--- hosts/server/open-webui.nix
+++ hosts/server/open-webui.nix
@@ -9,9 +9,11 @@
       ENABLE_OPENAI_API = "True";
       OPENAI_API_BASE_URL = "http://main.int:11343/v1";
       WEBUI_AUTH = "False";
-      CORIS_ALLOW_ORIGIN = "https://chat.slave.int";
+      CORS_ALLOW_ORIGIN = "https://chat.slave.int";
       WEBHOOK_URL = "chat.slave.int";
       REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-bundle.crt";
+      SEARXNG_URL = "https://search.slave.int/search?q=<query>";
+      SEARXNG_API_KEY = "";
     };
   };
 
