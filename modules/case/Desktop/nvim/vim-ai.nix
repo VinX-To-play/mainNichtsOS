026d03605fa8a9014381ef38f0e1a0cdd8f61687
VinX-To-play <v@lundborgs.de>
2026-09-09 18:41:14 +0200

    Update 2026-09-09 18:41:14

--- modules/case/Desktop/nvim/vim-ai.nix
+++ modules/case/Desktop/nvim/vim-ai.nix
@@ -27,7 +27,7 @@
         \   'model': 'deepseek-v4-flash',
         \   'endpoint_url': 'https://api.deepseek.com/v1/chat/completions',
         \   'auth_type': 'api-key',
-        \   'token_file_path': '/run/secrets/llm-apiKey/deepseek',
+        \   'api_key: $DEEPSEEK_API_KEY,
         \   },
         \ }
         
